b9017398add1569c5256e19c770ce8982a349745
olivia svensson <oliviateoliva@gmail.com>
2023-09-09 11:23:16 +0200

    add time layout

--- MyFrame.java
+++ MyFrame.java
@@ -4,20 +4,30 @@ import java.text.SimpleDateFormat;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.util.Calendar;
-imprt java.text.SimpleDateFormat;
+
 
 public class MyFrame extends JFrame {
 
     Calendar calendar;
     SimpleDateFormat timeFormat;
     JLabel timeLabel;
+    String time;
 
     MyFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Clock Program");
-        this.setLayout(new FLowLayout());
+        this.setLayout(new FlowLayout());
         this.setSize(350,200);
         this.setResizable(false);
+
+        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+
+        timeLabel = new JLabel();
+
+        time = timeFormat.format(Calendar.getInstance().getTime());
+        timeLabel.setText(time);
+
+        this.add(timeLabel);
         this.setVisible(true);
     }
 }
