02d69815bf401532eb46b675a9b6cd0a6685331a
olivia svensson <oliviateoliva@gmail.com>
2023-09-09 11:39:22 +0200

    get day of the week

--- MyFrame.java
+++ MyFrame.java
@@ -1,4 +1,6 @@
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.text.SimpleDateFormat;
 
 import javax.swing.JFrame;
@@ -10,7 +12,10 @@ public class MyFrame extends JFrame {
 
     Calendar calendar;
     SimpleDateFormat timeFormat;
+    SimpleDateFormat dayFormat;
     JLabel timeLabel;
+    JLabel dayLabel;
+    String day;
     String time;
 
     MyFrame() {
@@ -21,13 +26,41 @@ public class MyFrame extends JFrame {
         this.setResizable(false);
 
         timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        dayFormat = new SimpleDateFormat("E");
 
         timeLabel = new JLabel();
 
-        time = timeFormat.format(Calendar.getInstance().getTime());
-        timeLabel.setText(time);
+        timeLabel.setFont(new Font("Verdana",Font.PLAIN,50));
+        timeLabel.setForeground(new Color(0x00FF00));
+        timeLabel.setBackground(Color.black);
+       timeLabel.setOpaque(true);
 
+
+       dayLabel = new JLabel();
+       dayLabel.setFont(new Font("Arial", Font.PLAIN, 30));
         this.add(timeLabel);
+        this.add(dayLabel);
         this.setVisible(true);
+
+        setTime();
+    }
+
+    public void setTime() {
+        while(true) {
+            time = timeFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(time);
+
+               day = dayFormat.format(Calendar.getInstance().getTime());
+            dayLabel.setText(day);
+
+            try {
+            Thread.sleep(1000);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+        
     }
 }
